4425303a23a4d20eb09a62487d5a83114a6bde9d
natdanaix <103471625+natdanaix@users.noreply.github.com>
2023-07-12 19:07:06 +0700

    natdanai

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:layout_grid/pages/dash.dart';
+import 'package:layout_grid/pages/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DashPage(),
+      home: HomePage(),
     );
   }
 }
